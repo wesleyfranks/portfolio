5e2c59b338347ff8f3b6d692c66f1c8d0663dff0
Wesley Franks <code.wesleyfranks@gmail.com>
2024-10-09 13:09:04 -0500

    Updated placeholders.

--- src/data/timeline.jsx
+++ src/data/timeline.jsx
@@ -1,36 +1,50 @@
 export default [
   {
     year: 'Today',
-    title: 'YouTube Content Creator',
-    duration: '6 years',
+    title: 'Title: Needs Updating',
+    duration: '<1 year',
     details:
       'Details go here and so I am going to just type a bunch of stuff so that I can fill it in later',
   },
   {
-    year: 'Today',
-    title: 'YouTube Content Creator',
-    duration: '6 years',
+    year: '2024',
+    title: 'Title: Needs Updating',
+    duration: '<1 years',
+    details:
+      'Details go here and so I am going to just type a bunch of stuff so that I can fill it in later',
+  },
+  {
+    year: '2023',
+    title: 'Title: Needs Updating',
+    duration: '>1 year',
     details:
       'Details go here and so I am going to just type a bunch of stuff so that I can fill it in later',
   },
   {
     year: '2022',
-    title: 'YouTube Content Creator',
-    duration: '6 years',
+    title: 'Title: Needs Updating',
+    duration: '2 years',
     details:
       'Details go here and so I am going to just type a bunch of stuff so that I can fill it in later',
   },
   {
     year: '2021',
-    title: 'YouTube Content Creator',
-    duration: '6 years',
+    title: 'Title: Needs Updating',
+    duration: '3 years',
     details:
       'Details go here and so I am going to just type a bunch of stuff so that I can fill it in later',
   },
   {
     year: '2020',
-    title: 'YouTube Content Creator',
-    duration: '6 years',
+    title: 'Title: Needs Updating',
+    duration: '4 years',
+    details:
+      'Details go here and so I am going to just type a bunch of stuff so that I can fill it in later',
+  },
+  {
+    year: '2019',
+    title: 'Title: Needs Updating',
+    duration: '5 years',
     details:
       'Details go here and so I am going to just type a bunch of stuff so that I can fill it in later',
   },
