6aac314950bf43d2d8a9b6737a01aa6f3bf40e5b
Wesley Franks <code.wesleyfranks@gmail.com>
2024-10-16 19:15:58 -0500

    Updated FavIcon and mainly the timeline.

--- src/data/timeline.jsx
+++ src/data/timeline.jsx
@@ -1,52 +1,38 @@
 export default [
   {
     year: 'Today',
-    title: 'Title: Needs Updating',
-    duration: '<1 year',
-    details:
-      'Details go here and so I am going to just type a bunch of stuff so that I can fill it in later',
-  },
-  {
-    year: '2024',
-    title: 'Title: Needs Updating',
-    duration: '<1 years',
-    details:
-      'Details go here and so I am going to just type a bunch of stuff so that I can fill it in later',
-  },
-  {
-    year: '2023',
-    title: 'Title: Needs Updating',
+    title: 'Minimum Portfolio',
     duration: '>1 year',
     details:
-      'Details go here and so I am going to just type a bunch of stuff so that I can fill it in later',
+      'A website (this website) that showcases my Portfolio and provides a way for others to get in touch with me. Published using github actions and built in VSCode using ReactJS + Vite.',
   },
   {
-    year: '2022',
-    title: 'Title: Needs Updating',
+    year: '2015',
+    title: 'Keep Recipe',
+    duration: '8 years',
+    details:
+      'An android app written in Java at first. It then was re-written in Kotlin. A recipe app, think instagram but for recipes and reviews were similar to ebay reviews after buying a product. The idea behind this app was to allow you to build up a certain amount of recipes then you could order a volume 1 or recipes. As you grew your recipes and adapted others to become your own you could create a volume 2. These Keep Recipe volume cook books would have memories attached to the recipes you created. As most recipes are family recipes and have memories attached to them.',
+  },
+  {
+    year: '2013',
+    title: 'TagSpots',
     duration: '2 years',
     details:
-      'Details go here and so I am going to just type a bunch of stuff so that I can fill it in later',
+      'This app was only in development but it was meant to be a map based app that allowed you to generate spots that you tag and leave reviews, comments, etc. to help people find certain spots for camping, geocaching, etc.',
   },
   {
-    year: '2021',
-    title: 'Title: Needs Updating',
-    duration: '3 years',
+    year: '2012',
+    title: 'Stormy',
+    duration: '2 years',
     details:
-      'Details go here and so I am going to just type a bunch of stuff so that I can fill it in later',
+      'An android app that used the Dark Sky API to get weather. This was a tutorial project however, I adjusted this to allow using Zip codes to search for weather in your area. I also adjusted the UI to different color themes based on the temperature.',
   },
   {
-    year: '2020',
-    title: 'Title: Needs Updating',
-    duration: '4 years',
+    year: '2011',
+    title: 'Meglio',
+    duration: '1 years',
     details:
-      'Details go here and so I am going to just type a bunch of stuff so that I can fill it in later',
-  },
-  {
-    year: '2019',
-    title: 'Title: Needs Updating',
-    duration: '5 years',
-    details:
-      'Details go here and so I am going to just type a bunch of stuff so that I can fill it in later',
+      'This was my very first android app and my first dev project of anykind. It was a simple app that helped people identify their personal IT needs. If it had electricity running through it. I had a visual aid to assit with identifying it.',
   },
 ];
 
