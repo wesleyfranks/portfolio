448dff06f46be568bf98b86e011674fb568c9c30
wesleyfranks <code.wesleyfranks@gmail.com>
2024-11-03 11:43:41 -0600

    Fixed stack info for timeline and portfolio

--- src/data/portfolio.jsx
+++ src/data/portfolio.jsx
@@ -2,19 +2,19 @@ export default [
   {
     title: 'Portfolio',
     imgUrl: '/assets/projects/Portfolio.png',
-    stack: ['React', 'TailwindCSS', 'Vite'],
+    stack: ['React', 'TailwindCSS', 'Vite', 'npm'],
     link: 'https://wesleyfranks.com/',
   },
   {
     title: 'Net Take Home',
     imgUrl: '/assets/projects/NetTakeHome.png',
-    stack: ['React', 'TailwindCSS', 'Vite'],
+    stack: ['React', 'TailwindCSS', 'Vite', 'npm'],
     link: 'https://wesleyfranks-nettakehome.netlify.app/',
   },
   {
     title: 'Password Generator',
     imgUrl: '/assets/projects/Password-Generator.png',
-    stack: ['React', 'CSS', 'Vite'],
+    stack: ['React', 'TailwindCSS', 'Vite', 'npm'],
     link: 'https://wesleyfranks-passgen.netlify.app//',
   },
 ];
