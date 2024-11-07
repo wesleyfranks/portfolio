3969264e3cfe70fd52a4fb9829971217aa8a2d6b
wesleyfranks <code.wesleyfranks@gmail.com>
2024-11-07 14:34:58 -0600

    added links to the timeline items, clickable, new tab, no highjack

--- src/data/timeline.jsx
+++ src/data/timeline.jsx
@@ -5,6 +5,7 @@ export default [
     duration: '>1 year',
     platform: 'Web',
     stack: ['React', 'TailwindCSS', 'Vite', 'npm'],
+    link: 'https://wesleyfranks.com/',
     details:
       'A website (this website) that showcases my Portfolio and provides a way for others to get in touch with me. Published using github actions and built in VSCode using ReactJS + Vite. Which is hosted on Netlify. Credit to ForrestKnight (yes there are two "r"s) for the idea and YouTube Tutorial.',
   },
@@ -14,6 +15,7 @@ export default [
     duration: '>1 year',
     platform: 'Web',
     stack: ['React', 'TailwindCSS', 'Vite', 'npm'],
+    link: 'https://wesleyfranks-nettakehome.netlify.app/',
     details:
       'I created this app from complete scratch using what I learned from the password generator styling as it is simple. I was always trying to find a net salary calculator and could never find one that was what I needed. So, I decided to create one. This app calculates net take home of a gross salary and includes applicable taxes per state.',
   },
@@ -23,6 +25,7 @@ export default [
     duration: '<1 year',
     platform: 'Web',
     stack: ['React', 'CSS', 'Vite', 'npm'],
+    link: 'https://wesleyfranks-reactjs-todo.netlify.app/',
     details:
       'This web app was created as a tutorial project. I adapted the ability to add todo with the enter key. Adjusted it from an input to a textarea that allows for more text.',
   },
@@ -32,6 +35,7 @@ export default [
     duration: '2 years',
     platform: 'Web',
     stack: ['React', 'TailwindCSS', 'Vite', 'npm'],
+    link: 'https://wesleyfranks-passgen.netlify.app/',
     details:
       'This web app was created as a tutorial project that I adapted a feature for creating random word phrase based passwords using a random word generating API. I created this to solve a problem I saw with password generators not building phrase based passwords.',
   },
@@ -40,7 +44,8 @@ export default [
     title: 'Keep Recipe',
     duration: '8 years',
     platform: 'Android/iOS/Web',
-    stack: ['Kotlin', 'Swift', 'React', 'Vite', 'MongoDB'],
+    stack: ['Kotlin', 'Swift', 'Firebase'],
+    link: '',
     details:
       'A mobile app written in Java at first. It then was re-written in Kotlin for Android. I then found someone ot help with iOS using swift. The web app was planned for after finalizing mobile. A recipe app, think instagram but for recipes and reviews were similar to ebay reviews after buying a product. The idea behind this app was to allow you to build up a certain amount of recipes then you could order a volume 1 or recipes. As you grew your recipes and adapted others to become your own you could create a volume 2. These Keep Recipe volume cook books would have memories attached to the recipes you created. As most recipes are family recipes and have memories attached to them.',
   },
@@ -50,6 +55,7 @@ export default [
     duration: '2 years',
     platform: 'Android',
     stack: ['Java'],
+    link: '',
     details:
       'This app was only in development but it was meant to be a map based app that allowed you to generate spots that you tag and leave reviews, comments, etc. to help people find certain spots for camping, geocaching, etc.',
   },
@@ -59,6 +65,7 @@ export default [
     duration: '2 years',
     platform: 'Android',
     stack: ['Java'],
+    link: '',
     details:
       'An android app that used the Dark Sky API to get weather. This was a tutorial project however, I adjusted this to allow using Zip codes to search for weather in your area. I also adjusted the UI to different color themes based on the temperature.',
   },
@@ -68,6 +75,7 @@ export default [
     duration: '1 years',
     platform: 'Android',
     stack: ['Java'],
+    link: '',
     details:
       'This was my very first android app and my first dev project of anykind. It was a simple app that helped people identify their personal IT needs. If it had electricity running through it. I had a visual aid to assit with identifying it.',
   },
