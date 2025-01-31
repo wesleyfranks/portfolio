5163a3bb682b780f096a2bf49d07c42a3e5d1ee3
Wesley <code.wesleyfranks@gmail.com>
2025-01-31 11:55:54 -0600

    Added Linux+ cert in data and public/assetts/certs/linuxPNG

--- src/data/certs.jsx
+++ src/data/certs.jsx
@@ -1,4 +1,9 @@
+
 export default [
+  {
+    imgUrl: '/assets/certs/CTLinuxPlus.png',
+    url: 'https://www.credly.com/earner/earned/badge/369c1c23-c4a2-4780-8ba0-76d0c62b00ac',
+  },
   {
     imgUrl: '/assets/certs/CTSecPlus.png',
     url: 'https://www.credly.com/badges/6b04b79f-7460-45dd-89db-5b06fe28c515',
@@ -24,4 +29,3 @@ export default [
     url: 'https://learn.microsoft.com/api/credentials/share/en-us/wesleyfranks/9423ADAA1D1CFB40?sharingId=FA49F919AA26F908',
   },
 ]; 
-
